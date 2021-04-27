f5db3bceb6a2fe5e35059a502537aa115447ffa1
Soul <albert.lucass@hotmail.com>
2021-04-27 14:27:21 +0200

    etape 8 en cours

--- src/Table.js
+++ src/Table.js
@@ -4,12 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from './component/Button.jsx'
 import Cartes from "./component/Cartes";
 import StartGame from './component/StartGame.jsx'
-
-const cardArray = [
-  "KS", "QS", "JS", "AS", "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "0S",
-  "KD", "QD", "JD", "AD", "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "0D",
-  "KH", "QH", "JH", "AH", "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "0H",
-  "KC", "QC", "JC", "AC", "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "0C"];
+import cardsUtils from './utils/cardsUtils.js'
 
 class Table extends React.Component {
   constructor() {
@@ -26,20 +21,6 @@ class Table extends React.Component {
     }
     //tout commence a "0",pas de carte,game pas commencé
   }
-
-  rndCarte() {
-    let rndNumTemp = 0;
-    let rndCarteTemp = "";
-    rndNumTemp = Math.floor(Math.random() * 53);
-
-    if (rndNumTemp > 52) { rndNumTemp = rndNumTemp - 10 } else if (rndNumTemp < 1) { rndNumTemp = rndNumTemp + 10 }
-
-    rndCarteTemp = cardArray[rndNumTemp - 1];
-
-    return rndCarteTemp
-  }
-  //honnetement je suis pas sur,mais je pense que ça donne une carte au hazard dans les 52 et si la valeur est en dessous de 1 ça donne la carte 11
-  //car +10,et pareil avec la valeur au dessus de 52 avec -10
 
   onClickStop = () => {
     const cardSelectedDealer = this.rndCarte()
@@ -122,14 +103,6 @@ class Table extends React.Component {
     //this onclick add la carte ajouté par le bouton au total du player
   }
 
-  transformCardIntoInt(cardValue) {
-    if (cardValue === "K" || cardValue === "Q" || cardValue === "J" || cardValue === "A" || cardValue === "0") {
-      cardValue = "10"
-    }
-
-    return parseInt(cardValue)
-  }
-  //done la veleur 10 si la carte tiré est : roi,reine,valet,as ou 0 et la met en int
   startGame = () => {
     const cardSelected = this.rndCarte()
     const cardSelected2 = this.rndCarte()
